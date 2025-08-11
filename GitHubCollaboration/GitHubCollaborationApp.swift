c3deddea1d243ab91cb01a38797f2b4ca6ece283
Tamara Inbar <tamarainbar@gmail.com>
2025-08-11 09:46:57 -0400

    Initial Commit

---
+++ GitHubCollaboration/GitHubCollaborationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCollaborationApp.swift
+//  GitHubCollaboration
+//
+//  Created by Scholar on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubCollaborationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
